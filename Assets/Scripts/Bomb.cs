982bc22508c1da78044ca25074bc53b4b22a0842
Teemu Heinämäki <teeehe@utu.fi>
2016-01-30 13:41:23 +0200

    Particles for the bomb

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -12,7 +12,9 @@ public class Bomb : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (GetComponent<Rigidbody> ().velocity.magnitude < 1) {
+			Destroy (this.gameObject);
+		}
 	}
 
 	void OnCollisionEnter (Collision col) {
