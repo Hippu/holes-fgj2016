ae291a4b9196af38d432428b2496b5b30f28f804
Teemu Heinämäki <teeehe@utu.fi>
2016-01-30 14:34:46 +0200

    Save

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	public string nextLevel;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.tag == "Player") {
+			UnityEngine.SceneManagement.SceneManager.LoadScene (nextLevel);
+		}
+	}
+}
